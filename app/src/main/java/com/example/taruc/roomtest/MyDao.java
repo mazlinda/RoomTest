34b8a5d175a7ac25182f07e98630f0d57a9432c5
mazlinda <mazlindanm@tarc.edu.my>
2018-05-24 09:50:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/roomtest/MyDao.java
@@ -0,0 +1,15 @@
+package com.example.taruc.roomtest;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+
+/**
+ * Created by taruc on 23/5/2018.
+ */
+@Dao
+public interface MyDao {
+
+    @Insert
+    public void addUser(User user);
+
+}
